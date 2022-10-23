0c5f3b2ad0891f6f5fc424d66fc540fe65ab33cf
Sarbojit Bhattacharjee <70182981+Ariv1999@users.noreply.github.com>
2022-10-23 09:34:20 +0530

    Update to function Base Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,10 @@
 <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+function App() {
+  return(
+    <div>
+      This is a functionBase component.
+    </div>
+  )
+}
+export default App;
